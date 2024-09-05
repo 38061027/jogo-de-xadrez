de179896353ff4be326b9a5be7293da5ab0f4787
Matheus <matheus.fernandes-movida@outlook.com>
2024-09-05 13:30:56 -0300

    Fazendo alguns ajustes

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using tabuleiro;
+using xadrez;
 namespace xadrez_console
 {
 
@@ -7,10 +8,23 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
+
+            try
+            {
+
             Tabuleiro tab = new Tabuleiro(8,8);
+            tab.colocarPeca(new Torre(tab,Cor.Preta), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            tab.colocarPeca(new Rei(tab,Cor.Preta), new Posicao(0, 0));
+
+
             Tela.imprimirTabuleiro(tab);
 
             Console.WriteLine("Posição: " + tab);
+            }catch(TabuleioExeception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
---
+++ xadrez-console/tabuleiro/TabuleioExeception.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tabuleiro
+{
+    internal class TabuleioExeception : Exception
+    {
+        public TabuleioExeception(string msg):base (msg) { }
+    }
+}
